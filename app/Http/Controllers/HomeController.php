7764998644258002275d05be5a523a6bbbb1e053
Pawan Harendra Mishra <pawanharendramishra@Pawan.local>
2019-07-15 01:36:39 +0530

    define controller function for crud

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+use App\Imports\ImportCsv;
+use Maatwebsite\Excel\Facades\Excel;
+
+use App\User;
+use App\Contacts;
+use Dotenv\Regex\Success;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +33,72 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $data = DB::table('contacts')->where('user', '=', Auth::user()->email)->paginate(10);
+        return view('home', compact('data'))->with('i', (request()->input('page', 1) - 1) * 10);
+        // return view('home');
+    }
+
+    public function import()
+    {
+        Excel::import(new ImportCsv, request()->file('file'));
+        return back();
+    }
+
+    public function add()
+    {
+        return view('add');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required',
+            'pin' => 'required',
+            'address' => 'required',
+            'pin' => 'required'
+        ]);
+        $contact = new Contacts([
+            'user' => Auth::user()->email,
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'phone' => $request->get('phone'),
+            'address' => $request->get('address'),
+            'pin' => $request->get('pin'),
+        ]);
+        $contact->save();
+        return redirect()->route('home')->with('success', 'Record Added.');
+    }
+
+    public function edit($id)
+    {
+        $contact = Contacts::find($id);
+        return view('edit', compact('contact', 'id'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'pin' => 'required'
+        ]);
+        $contact = Contacts::find($id);
+        $contact->user = Auth::user()->email;
+        $contact->name = $request->get('name');
+        $contact->phone = $request->get('phone');
+        $contact->address = $request->get('address');
+        $contact->pin = $request->get('pin');
+        $contact->save();
+        return redirect()->route('home')->with('success', 'Record Updated.');
+    }
+
+    public function remove($id)
+    {
+        $contact = Contacts::find($id);
+        $contact->delete();
+        return redirect()->route('home')->with('success', 'Record Deleted.');
     }
 }
