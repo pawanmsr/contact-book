4ea922945e102be47eb00802a226beb2a5b3956b
Pawan Harendra Mishra <pawanharendramishra@Pawan.local>
2019-07-15 01:32:47 +0530

    define contacts table

---
+++ app/Contacts.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Contacts extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user', 'name', 'email', 'phone', 'address', 'pin'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+}
